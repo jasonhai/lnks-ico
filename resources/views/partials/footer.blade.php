56ec841ed4f3e6eb87e7c4ed84126761449714ca
Jason Hai <pmhai90@gmail.com>
2019-05-11 14:02:28 +0700

    Jason: updated telegram link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,9 +9,9 @@
         </div>
      </div>
     <ul class="icons">
-        <li><a href="https://twitter.com/slv_one" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+        <li><a href="https://twitter.com/slv_one" class="icon alt fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
         <!-- <li><a href="https://discord.gg/F8Kske8" class="icon alt fa-twitter"><span class="label">Discord</span></a></li> -->
-        <li><a href="https://github.com/slvone" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-        <li><a href="https://bit.ly/2JB25B7" class="icon alt fa-telegram"><span class="label">Telegram</span></a></li>
+        <li><a href="https://github.com/slvone" class="icon alt fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+        <li><a href="https://bit.ly/2JB25B7" class="icon alt fa-telegram" target="_blank"><span class="label">Telegram</span></a></li>
     </ul>
 </footer>
